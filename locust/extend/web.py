188699c659ca75c13be7fb01d4088291ce6397fe
jacexh <jace@xuh.me>
2016-05-14 16:01:24 +0800

    adjust svg style

--- locust/extend/web.py
+++ locust/extend/web.py
@@ -5,11 +5,15 @@ from flask import jsonify, make_response
 from locust.web import app, _sort_stats
 from locust import runners
 import pygal
+from pygal import style
+
+PYGAL_STYLE = style.NeonStyle
 
 
 @app.route("/chart/distribution.svg")
 def rps_svg():
-    line_chart = pygal.Bar()
+    line_chart = pygal.Bar(style=PYGAL_STYLE, title='Stats Distribution',
+                           legend_at_bottom=True, legend_at_bottom_columns=1)
     line_chart.x_labels = ["50%", "66%", "75%", "80%", "90%", "95%", "98%", "99%", "100%"]
 
     for s in _sort_stats(runners.locust_runner.request_stats):
@@ -29,15 +33,15 @@ def rps_svg():
 
 @app.route("/report.html")
 def report_html():
-    return """<!DOCTYPE html>
+    html = """<!DOCTYPE html>
 <html>
   <head>
-    <!-- ... -->
+    <title>Locust Test Report</title>
   </head>
   <body>
     <figure>
-      <embed type="image/svg+xml" src="/chart/distribution.svg" />
+      <embed type="image/svg+xml" src="/chart/distribution.svg" height="600"/>
     </figure>
   </body>
-</html>
-"""
+</html>"""
+    return html
