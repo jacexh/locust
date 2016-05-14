3acc6f373e2b39d59fead13b382b28bbd83573e7
jacexh <jace@xuh.me>
2016-05-14 14:10:56 +0800

    test show svg in html

--- locust/extend/web.py
+++ locust/extend/web.py
@@ -8,4 +8,6 @@ import pygal
 
 @app.route("/svg/rps.svg")
 def rps_svg():
-    return jsonify(dict(code="100"))
+    bar_chart = pygal.Bar()
+    bar_chart.add('Fibonacci', [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
+    return bar_chart.render_response()
