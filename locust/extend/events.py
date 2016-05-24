dec061d00dd33944585b1c4b33f7e61812f97d97
jacexh <jace@xuh.me>
2016-05-24 15:17:54 +0800

    support for reporting respone to influxdb

--- locust/extend/events.py
+++ locust/extend/events.py
@@ -6,21 +6,24 @@ try:
 except (ImportError, SyntaxError):
     import json
 from locust import events as ori_events
-import pika
+
+
+__all__ = ["config"]
 
 
 class Globals(object):
 
     rabbit_channel = None
     rabbit_exchange = None
+    influx_client = None
 
 
 class _Config(object):
 
-    def __init__(self):
-        self._report_to_rabbit = 0
+    @staticmethod
+    def enable_report_to_rabbit(exchange, url=None, **kwargs):
+        import pika
 
-    def enable_report_to_rabbit(self, exchange, url=None, **kwargs):
         if Globals.rabbit_channel is None:
             if url:
                 connection = pika.BlockingConnection(pika.URLParameters(url))
@@ -28,28 +31,56 @@ class _Config(object):
                 connection = pika.BlockingConnection(pika.ConnectionParameters(**kwargs))
             else:
                 raise ValueError("RabbitMQ connection parameters needed")
+
             Globals.rabbit_channel = connection.channel()
-        if Globals.rabbit_exchange is None:
             Globals.rabbit_exchange = exchange
-
-        if not self._report_to_rabbit:
             ori_events.request_success += report_to_rabbit
-            self._report_to_rabbit = 1
 
-    def disable_report_to_rabbit(self):
-        self._report_to_rabbit = 0
+    @staticmethod
+    def disable_report_to_rabbit():
         ori_events.request_success -= report_to_rabbit
+        Globals.rabbit_exchange = None
+        Globals.rabbit_channel = None
+
+    @staticmethod
+    def enable_report_to_influx(dsn=None, **kwargs):
+        import influxdb
+
+        if Globals.influx_client is None:
+            if dsn:
+                Globals.influx_client = influxdb.InfluxDBClient.from_DSN(dsn, **kwargs)
+            elif kwargs:
+                Globals.influx_client = influxdb.InfluxDBClient(**kwargs)
+            else:
+                raise ValueError("InfluxDB connection parameters needed")
+
+            ori_events.request_success += report_to_influx
+
+    @staticmethod
+    def disable_report_to_influx():
+        Globals.influx_client = None
+        ori_events.request_success -= report_to_influx
+
+
+def response_to_dict(request_type, name, response_time, response_length, **kwargs):
+    return {
+        "measurement": "locust",
+        "tags": {"name": name, "request_type": request_type},
+        "timestamp": int(time.time() * 1000),
+        "fields": {"response_time": response_time, "response_length": response_length}
+    }
 
 
 def report_to_rabbit(request_type, name, response_time, response_length, **kwargs):
-    response = {
-        "measurement": "locust",
-        "tags": {"name": name, "request_type": request_type},
-        "timestamp": int(time.time()*1000),
-        "fields": {"response_time": response_time, "response_length": response_length}
-    }
     if Globals.rabbit_exchange and Globals.rabbit_channel:
-        Globals.rabbit_channel.basic_publish(Globals.rabbit_exchange, "", body=json.dumps(response))
+        Globals.rabbit_channel.basic_publish(Globals.rabbit_exchange, "", body=json.dumps(response_to_dict(
+            request_type, name, response_time, response_length, **kwargs)))
+
+
+def report_to_influx(request_type, name, response_time, response_length, **kwargs):
+    if Globals.influx_client:
+        Globals.influx_client.write_points(
+            [response_to_dict(request_type, name, response_time, response_length, **kwargs)])
 
 
 config = _Config()
